51b4f362400b13aad74b38105d914be36303ac71
jaoferrari <joaoestudos1608@gmail.com>
2026-05-10 19:39:16 -0300

    Removendo arquivos do Lab 04 da pasta errada (Lab 02)

--- LAB_02_Typescript/src/leitura_prompt.ts
+++
@@ -1,8 +0,0 @@
-import promptSync from "prompt-sync";
-
-const prompt = promptSync();
-
-const nome: string = prompt("Digite seu nome: ");
-const idade: string = prompt("Digite sua idade: ");
-
-console.log(`Olá, ${nome}! Você tem ${idade} anos.`);
